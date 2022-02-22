06fe70791ada95b9be0800429a145660b5e1237d
Karen Douglas <kdindependence@gmail.com>
2022-02-22 08:54:50 -0600

    adding Log in Page

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ app.set('view engine', 'ejs');
 app.listen(5000);
 
 app.get('/' , (req, res) => {
-    res.render('index')
+    res.render('index', {title: 'Home'})
 });
 app.get('/about' , (req, res) => {
-    res.render('about')
+    res.render('about',  {title: 'About'})
 });
 app.get('/add-cards' , (req, res) => {
-    res.render('add-cards')
+    res.render('add-cards',  {title: 'Add Cards'})
 });
 app.get('/create-deck' , (req, res) => {
-    res.render('create')
+    res.render('create',  {title: 'Create Deck'})
+});
+app.get('/login' , (req, res) => {
+    res.render('login',  {title: 'Log in'})
 });
